b95462052a9d060535c2c283139d6a29fbc45bea
Abozar Gardize <sayed.gardize@motopp.nl>
2025-05-22 09:37:07 +0200

    add new file

--- db/database.py
+++ db/database.py
@@ -1,29 +1,4 @@
 
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker, declarative_base
-
-
-# # SQLite URL for local file database
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./marketplace.db"
-
-# # Create engine with SQLite, allow multiple threads
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
-
-# # Session class to create database sessions
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# # Base class to create models from
-# Base = declarative_base()
-
-# # Dependency to get DB session (for FastAPI or other use)
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
  
