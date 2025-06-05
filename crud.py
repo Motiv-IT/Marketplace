9da624a82de8793f4d7e79309a7651c03fe97a99
Tina Shahedi <t.shahedi@uu.nl>
2025-06-05 14:11:16 +0200

    Revise bug in rating

--- crud.py
+++ crud.py
@@ -18,10 +18,11 @@ def create_rating(db: Session, rating: RatingCreate, current_user: int):
     # Prevent duplicate rating by same user on same transaction
     existing_rating = db.query(DbRating).filter(
         DbRating.transaction_id == rating.transaction_id,
-        # DbRating.rater_id == current_user
+        DbRating.rater_id == current_user
     ).first()
     if existing_rating:
-        raise HTTPException(status_code=400, detail="You have already rated this transaction")
+        raise HTTPException(status_code=400, detail="Rating already exists for this transaction by this user")  
+        
 
     # Determine who is being rated
     if current_user == transaction.buyer_id:
